84f9bde472b6d424cff8400300aa98d9d14b8aca
diogomarin <diogoamarin@gmail.com>
2024-03-15 02:06:56 -0300

    advanced change in admin

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -2,3 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
+from . models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class Productsdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    
+
